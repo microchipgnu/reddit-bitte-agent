f82271067b286fee8e645aae47ed06841b495117
Luís Freitas <microchipgnu@gmail.com>
2024-10-25 17:17:13 +0100

    add vercel-url

--- src/app/api/ai-plugin/route.ts
+++ src/app/api/ai-plugin/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { DEPLOYMENT_URL } from "vercel-url";
 
 
 const key = JSON.parse(process.env.BITTE_KEY || "{}");
@@ -18,7 +19,7 @@ export async function GET() {
         },
         servers: [
             {
-                url: config.url,
+                url: config.url || DEPLOYMENT_URL,
             },
         ],
         "x-mb": {
